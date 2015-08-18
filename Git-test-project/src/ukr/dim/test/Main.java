77009872aa57a0051b4d55cc62f28b10f48f5a4c
Dima <dima.otenko@mail.ru>
2015-08-18 15:32:00 +0300

    add 1 !

--- Git-test-project/src/ukr/dim/test/Main.java
+++ Git-test-project/src/ukr/dim/test/Main.java
@@ -3,7 +3,7 @@ package ukr.dim.test;
 public class Main {
 
 	public static void main(String[] args){
-		System.out.println("Hellow frob Git Hub!!!");
+		System.out.println("Hellow frob Git Hub!!!!");
 
 	}
 
